d72100c866ff27a1a8811323b3ce03271d7be980
Mehul Nawal <your.email@example.com>
2026-02-24 17:04:50 +0530

    update done

--- client/src/api/axiosProduct.api.jsx
+++ client/src/api/axiosProduct.api.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const productAxios = axios.create({
+export const productAxios = axios.create({
     baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1/admin/products`,
     withCredentials: true
 });
-
-export default productAxios;
